2ac1488e55b822bc6a2f1e1c197cba8d92af52de
Mainak Jas <jasmainak@users.noreply.github.com>
2018-09-05 13:52:10 -0400

    MRG: some misc fixes (#8)
    * Version
    
    * Add flake8 checker
    
    * Add more dependencies

--- setup.py
+++ setup.py
@@ -2,20 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+import ephypype
 
+VERSION = ephypype.__version__
 
-setup(
-    name='ephypype',
-    version='0.0.2',
-    packages=find_packages(),
-    author=['David Meunier',
-            'Annalisa Pascarella',
-            'Dmitrii Altukhov'],
-    description='Definition of functions used\
-                 as Node for electrophy (EEG/MEG)\
-                 pipelines within nipype framework',
-    lisence='BSD 3',
-    install_requires=['mne>=0.14',
-                      'nipype',
-                      'configparser']
-)
+if __name__ == "__main__":
+    setup(
+        name='ephypype',
+        version=VERSION,
+        packages=find_packages(),
+        author=['David Meunier',
+                'Annalisa Pascarella',
+                'Dmitrii Altukhov'],
+        description='Definition of functions used\
+                     as Node for electrophy (EEG/MEG)\
+                     pipelines within nipype framework',
+        lisence='BSD 3',
+        install_requires=['mne>=0.14',
+                          'nipype',
+                          'configparser']
+    )
