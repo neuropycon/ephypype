9b4afc1ff0586790807c2178f88a3a1390de91e8
David Meunier <david_meunier_79@hotmail.fr>
2018-09-11 10:37:36 +0200

    added path to the file __init__ to get the version

--- setup.py
+++ setup.py
@@ -2,14 +2,24 @@
 # -*- coding: utf-8 -*-
 """Setup configuration."""
 
+import os
 from setuptools import setup, find_packages
+
+VERSION = None
+with open(os.path.join('ephypype', '__init__.py'), 'r') as fid:
+    for line in (line.strip() for line in fid):
+        if line.startswith('__version__'):
+            VERSION = line.split('=')[1].strip().strip('\'')
+            break
+        
+if VERSION is None:
+    raise RuntimeError('Could not determine version')
 
 
 if __name__ == "__main__":
     setup(
         name='ephypype',
-        # version=VERSION,
-        version='0.1.dev0',
+        version=VERSION,
         packages=find_packages(),
         author=['David Meunier',
                 'Annalisa Pascarella',
