75fa5e6066def51510992c3f5505a8b6b2f43c85
annapasca <a.pascarella@iac.cnr.it>
2018-09-12 14:00:24 +0200

    change testing dataset

--- ephypype/tests/test_import_ctf.py
+++ ephypype/tests/test_import_ctf.py
@@ -3,10 +3,9 @@ import mne
 import os.path as op
 
 from ephypype.import_ctf import convert_ds_to_raw_fif  # noqa
-from mne.datasets.brainstorm import bst_auditory  # noqa
 
-data_path = bst_auditory.data_path()
-ds_fname = op.join(data_path, 'MEG', 'bst_auditory', 'S01_AEF_20131218_01.ds')
+data_path = mne.datasets.testing.data_path()
+ds_fname = op.join(data_path, 'CTF', 'testdata_ctf.ds')
 
 
 def test_convert_ds_to_raw_fif():
