8b78318fa895893b1c8cd4449adc555c8f2e60d2
David Meunier <david_meunier_79@hotmail.fr>
2018-09-27 10:28:50 +0200

    pytest raises

--- ephypype/tests/test_spectral.py
+++ ephypype/tests/test_spectral.py
@@ -3,6 +3,10 @@
 import numpy as np
 
 import os
+import shutil
+
+import pytest
+
 from ephypype.spectral import (compute_spectral_connectivity,
                                compute_and_save_multi_spectral_connectivity)  # noqa
 
@@ -37,7 +41,7 @@ ts_mat_trials = np.random.rand(nb_trials, nb_ROI, time_length) * \
 
 def test_compute_spectral_connectivity_one_trial():
     """Test compute_spectral_connectivity plv with one trial."""
-    try:
+    with pytest.raises(SystemExit, message="OK caught SystemExit as expected for plv with one trial") as exception_one_plv:
         res_plv = compute_spectral_connectivity(
             data=ts_mat,
             con_method="plv",
@@ -46,24 +50,8 @@ def test_compute_spectral_connectivity_one_trial():
             fmax=fmax,
             sfreq=sfreq,
             gathering_method='mean')
-    except SystemExit:
-        print("OK caught SystemExit as expected for plv with one trial")
-        assert True
 
-    try:
-        res_plv = compute_spectral_connectivity(
-            data=ts_mat_trials,
-            con_method="plv",
-            mode="multitaper",
-            fmin=fmin,
-            fmax=fmax,
-            sfreq=sfreq,
-            gathering_method='mean')
-    except SystemExit:
-        print("SystemExit should not raise excepction for plv with several trials")
-        assert False
-
-    #assert np.all(res_mean <= res_max), "error, all mean values should be lower than max values"
+    print(exception_one_plv)
 
 
 def test_compute_spectral_connectivity_mean_max():
@@ -93,12 +81,14 @@ def test_compute_spectral_connectivity_mean_max():
     assert np.all(
         res_mean <= res_max), "error, all mean values should be lower than max values"
 
-# testing compute_and_save_multi_spectral_co
+# testing test_compute_and_save_multi_spectral_connectivity
 
-os.makedirs("/tmp/ephypype_test")
+tmp_dir = "/tmp/ephypype_test"
 
 
 def test_compute_and_save_multi_spectral_connectivity():
+
+    os.makedirs(tmp_dir)
 
     compute_and_save_multi_spectral_connectivity(
         all_data=ts_mat_trials,
@@ -108,11 +98,19 @@ def test_compute_and_save_multi_spectral_connectivity():
         fmax=fmax,
         sfreq=sfreq,
         gathering_method='mean',
-        save_dir="/tmp/ephypype_test")
+        save_dir=tmp_dir)
+
+    # list all created files
+    tmp_files = os.listdir(tmp_dir)
+
+    assert len(tmp_files) == nb_trials, "There should an equal number of files {} as trials {}".format(
+        len(tmp_files), nb_trials)
+
+    shutil.rmtree(tmp_dir)
 
 if __name__ == '__main__':
 
-    # test_compute_spectral_connectivity_one_trial()
+    test_compute_spectral_connectivity_one_trial()
     # test_compute_spectral_connectivity_mean_max()
 
-    test_compute_and_save_multi_spectral_connectivity()
+    # test_compute_and_save_multi_spectral_connectivity()
