5b4f10911c7c2791c94c3f39d28438d425acbcdf
annapasca <a.pascarella@iac.cnr.it>
2018-10-24 16:09:57 +0200

    fix flake8

--- ephypype/gather/gather_conmats.py
+++ ephypype/gather/gather_conmats.py
@@ -19,7 +19,7 @@ def natural_keys(text):
     http://nedbatchelder.com/blog/200712/human_sorting.html
     (See Toothy's implementation in the comments)
     """
-    return [atoi(c) for c in re.split('(\d+)', text)]
+    return [atoi(c) for c in re.split(r'(\d+)', text)]
 
 
 def return_full_mat(mat, elec_labels, all_elec_labels):
