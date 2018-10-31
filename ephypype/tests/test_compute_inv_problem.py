4abbc37a9cd52e522c6499dc1b52ee89fbdea2e8
annapasca <a.pascarella@iac.cnr.it>
2018-10-31 14:40:37 +0100

    add test_compute_cov_identity

--- ephypype/tests/test_compute_inv_problem.py
+++ ephypype/tests/test_compute_inv_problem.py
@@ -1,7 +1,9 @@
 """Test compute_inv_problem."""
 
 import mne
+import numpy as np
 from ephypype.compute_inv_problem import compute_noise_cov
+from ephypype.compute_inv_problem import compute_cov_identity
 
 import matplotlib
 matplotlib.use('Agg')  # for testing don't use X server
@@ -29,3 +31,20 @@ def test_compute_noise_cov():
     picks = mne.pick_types(raw.info, meg=True, ref_meg=False, exclude='bads')
 
     assert len(picks) == noise_cov['dim']
+
+
+def test_compute_cov_identity():
+    """Test compute identity noise covariance data"""
+
+    # compute I
+    noise_cov_fpath = compute_cov_identity(raw_fname)
+    identity_cov = mne.read_cov(noise_cov_fpath)
+
+    dim = identity_cov['dim']
+    assert np.all(identity_cov['data'] == np.identity(dim))
+
+    # read raw data
+    raw = mne.io.read_raw_fif(raw_fname)
+    picks = mne.pick_types(raw.info, meg=True, ref_meg=False, exclude='bads')
+
+    assert len(picks) == identity_cov['dim']
