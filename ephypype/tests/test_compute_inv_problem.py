874737f5e32675e8d4fa528591021c4d4625231b
annapasca <a.pascarella@iac.cnr.it>
2018-11-05 13:42:16 +0100

    use MNE test data

--- ephypype/tests/test_compute_inv_problem.py
+++ ephypype/tests/test_compute_inv_problem.py
@@ -2,14 +2,16 @@
 
 import mne
 import numpy as np
+import os.path as op
 from ephypype.compute_inv_problem import compute_noise_cov
 from ephypype.compute_inv_problem import compute_cov_identity
 
 import matplotlib
 matplotlib.use('Agg')  # for testing don't use X server
 
-data_path = mne.datasets.sample.data_path()
-filter_raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
+data_path = mne.datasets.testing.data_path()
+raw_fname = op.join(data_path, 'MEG', 'sample',
+                    'sample_audvis_trunc_raw.fif')
 
 
 def test_compute_noise_cov():
@@ -17,10 +19,10 @@ def test_compute_noise_cov():
     of raw data."""
 
     # read raw data
-    raw = mne.io.read_raw_fif(filter_raw_fname)
+    raw = mne.io.read_raw_fif(raw_fname)
 
     # save short segment of raw data
-    segment_raw_fname = filter_raw_fname.replace('raw.fif', '0_10s_raw.fif')
+    segment_raw_fname = raw_fname.replace('raw.fif', '0_10s_raw.fif')
     raw.save(segment_raw_fname, tmin=0, tmax=10, overwrite=True)
 
     noise_cov_fpath = compute_noise_cov('', segment_raw_fname)
@@ -36,14 +38,14 @@ def test_compute_cov_identity():
     """Test compute identity noise covariance data"""
 
     # compute I
-    noise_cov_fpath = compute_cov_identity(filter_raw_fname)
+    noise_cov_fpath = compute_cov_identity(raw_fname)
     identity_cov = mne.read_cov(noise_cov_fpath)
 
     dim = identity_cov['dim']
     assert np.all(identity_cov['data'] == np.identity(dim))
 
     # read raw data
-    raw = mne.io.read_raw_fif(filter_raw_fname)
+    raw = mne.io.read_raw_fif(raw_fname)
     picks = mne.pick_types(raw.info, meg=True, ref_meg=False, exclude='bads')
 
     assert len(picks) == identity_cov['dim']
