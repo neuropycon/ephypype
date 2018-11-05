27d440304d9318d1cfbe9cea4d3fbb8123791b52
annapasca <a.pascarella@iac.cnr.it>
2018-11-05 11:19:12 +0100

    change Dropbox link

--- ephypype/datasets.py
+++ ephypype/datasets.py
@@ -10,8 +10,8 @@ from mne.datasets.utils import _fetch_file
 
 
 def fetch_omega_dataset(base_path):
-    src_url = ('https://www.dropbox.com/sh/8pjuh893teeweg7/'
-               'AADg621Z32sr8w3xaTrsgeWja?dl=1')
+    src_url = ('https://www.dropbox.com/sh/feqtjna1ymok445/'
+               'AACStCwNgIlG3VcePkkTALgBa?dl=1')
 
     data_path = os.path.join(base_path, 'sample_BIDS_omega')
     target = os.path.join(base_path, 'sample_BIDS_omega.zip')
@@ -21,6 +21,6 @@ def fetch_omega_dataset(base_path):
             _fetch_file(src_url, target)
         zf = zipfile.ZipFile(target, 'r')
         print('Extracting files. This may take a while ...')
-        zf.extractall(path=os.path.join(data_path, 'sub-0003'))
+        zf.extractall(path=data_path)
         os.remove(target)
     return os.path.abspath(data_path)
