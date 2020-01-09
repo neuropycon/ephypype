8194927567bbda076364d05f4f111817af95510a
annapasca <a.pascarella@iac.cnr.it>
2020-01-09 15:28:33 +0100

    reorganize module

--- ephypype/compute_inv_problem.py
+++ ephypype/compute_inv_problem.py
@@ -15,14 +15,12 @@ from mne.evoked import write_evokeds, read_evokeds
 from mne.minimum_norm import make_inverse_operator, apply_inverse_raw
 from mne.minimum_norm import apply_inverse_epochs, apply_inverse
 from mne.beamformer import apply_lcmv_raw, make_lcmv
-from mne import get_volume_labels_from_src
 from mne import compute_raw_covariance, pick_types, write_cov
 
 from nipype.utils.filemanip import split_filename as split_f
 
 from .preproc import _create_reject_dict
-from .source_space import _create_MNI_label_files
-from .import_data import write_hdf5
+from .source_estimate import _process_stc
 
 
 def compute_noise_cov(fname_template, raw_filename):
@@ -313,43 +311,9 @@ def _compute_inverse_solution(raw_filename, sbj_id, subjects_dir, fwd_filename,
                                 buffer_size=1000,
                                 pick_ori=pick_ori)  # None 'normal'
 
-    if not isinstance(stc, list):
-        print('***')
-        print(('stc dim ' + str(stc.shape)))
-        print('***')
-
-        stc = [stc]
-    else:
-        print('***')
-        print(('len stc %d' % len(stc)))
-        print('***')
-
-    print('**************************************************************')
-    print('all_src_space: {}'.format(all_src_space))
-    print('ROIs_mean: {}'.format(ROIs_mean))
-    print('**************************************************************')
-    if all_src_space:
-        stc_data = list()
-        stc_file = op.abspath(basename + '_stc.hdf5')
-
-        for i in range(len(stc)):
-            stc_data.append(stc[i].data)
-
-        write_hdf5(stc_file, stc_data, dataset_name='stc_data')
-
-    if ROIs_mean:
-        label_ts, labels_file, label_names_file, label_coords_file = \
-            _compute_mean_ROIs(stc, sbj_id, subjects_dir, parc,
-                               forward, aseg, is_fixed)
-
-        ts_file = op.abspath(basename + '_ROI_ts.npy')
-        np.save(ts_file, label_ts)
-
-    else:
-        ts_file = stc_file
-        labels_file = ''
-        label_names_file = ''
-        label_coords_file = ''
+    ts_file, label_ts, labels_file, label_names_file, label_coords_file = \
+        _process_stc(stc, basename, sbj_id, subjects_dir, parc, forward,
+                     aseg, is_fixed, all_src_space=False, ROIs_mean=True)
 
     return ts_file, labels_file, label_names_file, \
         label_coords_file
@@ -420,77 +384,9 @@ def _compute_LCMV_inverse_solution(raw_filename, sbj_id, subjects_dir,
     # apply spatial filter
     stc = apply_lcmv_raw(raw, filters, max_ori_out='signed')
 
-    print('**************************************************************')
-    print('all_src_space: {}'.format(all_src_space))
-    print('ROIs_mean: {}'.format(ROIs_mean))
-    print('**************************************************************')
-    if all_src_space:
-        stc_data = list()
-        stc_file = op.abspath(basename + '_stc.hdf5')
-
-        for i in range(len(stc)):
-            stc_data.append(stc[i].data)
-
-        write_hdf5(stc_file, stc_data, dataset_name='stc_data')
-
-    if ROIs_mean:
-        label_ts, labels_file, label_names_file, label_coords_file = \
-            _compute_mean_ROIs(stc, sbj_id, subjects_dir, parc,
-                               forward, False, is_fixed)
-
-        ts_file = op.abspath(basename + '_ROI_ts.npy')
-        np.save(ts_file, label_ts)
-
-    else:
-        ts_file = stc_file
-        labels_file = ''
-        label_names_file = ''
-        label_coords_file = ''
+    ts_file, label_ts, labels_file, label_names_file, label_coords_file = \
+        _process_stc(stc, basename, sbj_id, subjects_dir, parc, forward,
+                     False, is_fixed, all_src_space=False, ROIs_mean=True)
 
     return ts_file, labels_file, label_names_file, \
         label_coords_file
-
-
-def _compute_mean_ROIs(stc, sbj_id, subjects_dir, parc,
-                       forward, aseg, is_fixed):
-    # these coo are in MRI space and we have to convert them to MNI space
-    labels_cortex = mne.read_labels_from_annot(sbj_id, parc=parc,
-                                               subjects_dir=subjects_dir)
-
-    print(('\n*** %d ***\n' % len(labels_cortex)))
-
-    src = forward['src']
-
-    # allow_empty : bool -> Instead of emitting an error, return all-zero time
-    # courses for labels that do not have any vertices in the source estimate
-
-    if is_fixed:
-        mode = 'mean_flip'
-    else:
-        mode = 'mean'
-
-    label_ts = mne.extract_label_time_course(stc, labels_cortex, src,
-                                             mode=mode,
-                                             allow_empty=True,
-                                             return_generator=False)
-
-    # save results in .npy file that will be the input for spectral node
-    print('\n*** SAVE ROI TS ***\n')
-    print((len(label_ts)))
-
-    if aseg:
-        print(sbj_id)
-        labels_aseg = get_volume_labels_from_src(src, sbj_id, subjects_dir)
-        labels = labels_cortex + labels_aseg
-    else:
-        labels = labels_cortex
-        labels_aseg = None
-
-    print((labels[0].pos))
-    print((len(labels)))
-
-    labels_file, label_names_file, label_coords_file = \
-        _create_MNI_label_files(forward, labels_cortex, labels_aseg,
-                                sbj_id, subjects_dir)
-
-    return label_ts, labels_file, label_names_file, label_coords_file
