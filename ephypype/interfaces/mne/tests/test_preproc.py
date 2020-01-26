bb4226eda7f892a9564b9503a6ba7d22a46ce2db
Annalisa Pascarella <a.pascarella@iac.cnr.it>
2020-01-26 10:27:41 +0100

    # This is a combination of 3 commits. (#78)
    # The first commit's message is:
    fix _preprocess_set_ica_comp_fif_to_ts
    
    # The 2nd commit message will be skipped:
    
    #	split_f -> split_filename
    
    # The 3rd commit message will be skipped:
    
    #	added test for set_IC_co_preprocess_set_ica_comp_fif_to_tsmponents

--- ephypype/interfaces/mne/tests/test_preproc.py
+++ ephypype/interfaces/mne/tests/test_preproc.py
@@ -3,9 +3,12 @@
 import mne
 import pytest
 import os.path as op
+import numpy as np
 import nipype.pipeline.engine as pe
 from ephypype.interfaces.mne.preproc import CreateEp, PreprocFif, CompIca
 from ephypype.interfaces.mne.preproc import DefineEpochs
+from ephypype.preproc import _preprocess_set_ica_comp_fif_to_ts
+
 
 import matplotlib
 matplotlib.use('Agg')  # for testing don't use X server
@@ -97,3 +100,43 @@ def test_compute_ica():
     raw_cleaned = mne.io.read_raw_fif(ica_node.result.outputs.ica_file)
 
     assert raw.get_data().shape == raw_cleaned.get_data().shape
+
+
+def test_set_IC_components():
+    """Test compute ICA on raw data."""
+
+    ecg_ch_name = 'ECG'
+    eog_ch_name = 'HEOG, VEOG'
+    variance = 0.5
+    reject = dict(mag=5e-12, grad=5000e-13)
+
+    # read raw data
+    raw = mne.io.read_raw_fif(raw_fname)
+
+    # save short segment of raw data
+    segment_raw_fname = raw_fname.replace('raw.fif', '0_60s_raw.fif')
+    raw.save(segment_raw_fname, tmin=0, tmax=60, overwrite=True)
+
+    # create CompIca node to compute ica on raw data
+    ica_node = pe.Node(interface=CompIca(), name='ica')
+    ica_node.inputs.fif_file = segment_raw_fname
+    ica_node.inputs.ecg_ch_name = ecg_ch_name
+    ica_node.inputs.eog_ch_name = eog_ch_name
+    ica_node.inputs.n_components = variance
+    ica_node.inputs.reject = reject
+
+    ica_node.run()
+
+    n_comp_exclude = {'sample': {'run': [0, 1, 2]}}
+    ts_file, chs_file, chs_n_file, sf = _preprocess_set_ica_comp_fif_to_ts(
+        ica_node.result.outputs.ica_file, 'sample', n_comp_exclude, True)
+
+    assert ts_file
+    assert chs_file
+    assert chs_n_file
+    assert sf
+
+    data = np.load(ts_file)
+
+    idx = mne.pick_types(raw.info, meg=True)
+    assert raw.get_data()[idx, :].shape == data.shape
